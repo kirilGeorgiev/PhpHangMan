a38a00da4393c2476392354fc80c6a71aeb92c1a
Kiril Georgiev <kiril.g.georgiev90@gmail.com>
2014-06-24 12:49:24 +0300

    Renamed WordClass.php to WordHandleClass.php and added some methods in it

--- WordHandleClass.php
+++ WordHandleClass.php
@@ -5,24 +5,41 @@ class WordHandle extends RandomWord{
 	private $letterGuesses; 
 	private $charInput;
 	private $word; 
+	private $lastGuess; 
 	
 	function newWordHandle(){
 		$word = new RandomWord;
 		iniGuessPartString();
 	}
-	
 	function iniGuessPartString(){
 		for ($i=0; $i <strlen($this->word) ; $i++) { 
 			$this->guessedPart[$i] = '_';
 		}
 	}
+	function getGuessedPart(){
+		return $this->guessedPart; 
+	}
+	function getLastGuessResult(){
+		return $this->lastGuess; 
+	}
 	
 	function setCharInPut($inputChar){
 		$inputChar = trim($inputChar);
 		$inputChar = substr($input_string, 0 , 1); 
-		$this->charInput = $inputChar ; 
+		$this->charInput = $inputChar ;
 	}
 	function isGuessTrue(){
+		
+		for($i = 0 ; i < strlen($this->word); $i++){
+			if($this->word[$i] == $this->charInput){
+				$this->lastGuess = TRUE;
+				$this->guessedPart[$i] = $this->charInput; 
+			}
+			else{
+				$this->lastGuess = FALSE; 
+				$this-> letterGuesses = $this->letterGuesses.$this->charInput; 
+			}
+		}
 		
 	}
 	
